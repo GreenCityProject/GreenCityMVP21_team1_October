1c6c46f24901765b95187ad7810f244ee3d53e8c
Serhii Zamorii <sezamua@gmail.com>
2024-11-23 20:51:26 +0200

    Enum PlaceStatus was created, property status was added to Place class, some OneToMany links properties were removed from Place class.

--- dao/src/main/java/greencity/entity/Place.java
+++ dao/src/main/java/greencity/entity/Place.java
@@ -9,8 +9,6 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.validator.constraints.Length;
 
-import java.util.List;
-
 @Entity
 @Table(name = "places")
 @NoArgsConstructor
