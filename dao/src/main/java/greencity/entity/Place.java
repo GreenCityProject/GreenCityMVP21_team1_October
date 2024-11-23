30d39498f45fa26db04b1736d0250269b19768b8
Serhii Zamorii <sezamua@gmail.com>
2024-11-23 15:53:12 +0200

    Base classes were created: Place, OpenHours, BreakTime, DiscountValue, WeekDay. Was added link from Comment to Place. Some changes in Location were made.

---
+++ dao/src/main/java/greencity/entity/Place.java
@@ -0,0 +1,61 @@
+package greencity.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.List;
+
+@Entity
+@Table(name = "places")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Builder
+public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Length(min = 1, max = 30, message = "Place name should be from 1 to 30 characters long")
+    @Column(unique = true)
+    private String name;
+
+    private Double rate;
+
+    @ManyToOne
+    @NotNull(message = "Place location can't be null!")
+    @OnDelete(action = OnDeleteAction.RESTRICT)
+    private Location location;
+
+    @ManyToOne
+    @NotNull(message = "Place category can't be null!")
+    @OnDelete(action = OnDeleteAction.RESTRICT)
+    private Category category;
+
+    @ManyToOne
+    @NotNull(message = "Place author can't be null!")
+    @OnDelete(action = OnDeleteAction.RESTRICT)
+    private User author;
+
+    // These three OneToMany properties below maybe should be moved to Dto in future
+
+    @OneToMany
+    private List<OpenHours> openHoursList;
+
+    @OneToMany
+    private List<DiscountValue> discountValues;
+
+    @OneToMany
+    private List<Comment> comments;
+}
