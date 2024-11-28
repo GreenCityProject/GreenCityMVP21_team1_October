040a45de471f752fc3e92c1edc94f0cc0231e929
Andrii-Danylenko <ineedmoreweekends@gmail.com>
2024-11-28 20:49:44 +0200

    Added rest of needed functionality. Reviews are more than welcome

--- core/src/main/java/greencity/controller/FavoritePlaceController.java
+++ core/src/main/java/greencity/controller/FavoritePlaceController.java
@@ -2,6 +2,8 @@ package greencity.controller;
 
 import greencity.annotations.CurrentUser;
 import greencity.constant.HttpStatuses;
+import greencity.dto.place.FavoritePlaceDto;
+import greencity.dto.place.PlaceByBoundsDto;
 import greencity.dto.user.UserVO;
 import greencity.service.FavoritePlaceService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,10 +12,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/favorite_place")
@@ -21,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FavoritePlaceController {
     private FavoritePlaceService favoritePlaceService;
 
-    @Operation(description = "Delete favorite place by userEmail")
+    @Operation(description = "Delete favorite place by user email")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
             @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST),
@@ -33,5 +34,47 @@ public class FavoritePlaceController {
             @Parameter(hidden = true) @CurrentUser UserVO userVO, @PathVariable("placeId") long placeId
     ) {
         return ResponseEntity.ok(favoritePlaceService.deleteFavoritePlaceById(placeId, userVO));
+    }
+
+    @Operation(description = "Get favorite place with Coordinate by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+            @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
+            @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND)
+    })
+    @GetMapping("/favorite/{placeId}")
+    public ResponseEntity<PlaceByBoundsDto> getFavoritePlaceWithCoordinateUsingGET(
+            @PathVariable("placeId") long placeId,
+            @Parameter(hidden = true) @CurrentUser UserVO userVO
+    ) {
+        return ResponseEntity.ok(favoritePlaceService.getFavoriteByUserAndPlaceId(userVO, placeId));
+    }
+
+    @Operation(description = "Get list favorite places by user email")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED)
+    })
+    @GetMapping
+    public ResponseEntity<List<PlaceByBoundsDto>> getFavoritePlaceWithCoordinateUsingGET(
+            @Parameter(hidden = true) @CurrentUser UserVO userVO
+    ) {
+        return ResponseEntity.ok(favoritePlaceService.getAllFavoritesByUser(userVO));
+    }
+
+    @Operation(description = "Update user favorite place")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+            @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
+            @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND)
+    })
+    @PutMapping
+    public ResponseEntity<FavoritePlaceDto> updateUsingPUT3(
+            @RequestBody FavoritePlaceDto favoritePlaceDto,
+            @Parameter(hidden = true) @CurrentUser UserVO userVO) {
+        favoritePlaceService.updateFavoritePlace(favoritePlaceDto, userVO);
+        return ResponseEntity.ok(favoritePlaceDto);
     }
 }
--- dao/src/main/java/greencity/repository/FavoritePlaceRepository.java
+++ dao/src/main/java/greencity/repository/FavoritePlaceRepository.java
@@ -5,12 +5,33 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface FavoritePlaceRepository extends JpaRepository<Place, Long> {
     @Modifying
-    @Query(value = "DELETE FROM users_favorite_places WHERE place_id = ?2 and " +
-                   "user_id = (SELECT id FROM users WHERE email = ?1)", nativeQuery = true)
+    @Query(value = "DELETE FROM users_favorite_places WHERE place_id = ?2 AND " +
+                   "user_id = (SELECT id FROM users WHERE email = ?1)",
+            nativeQuery = true)
     void deleteByUserEmailAndPlaceId(String userEmail, long placeId);
 
-    @Query(value = "SELECT EXISTS(SELECT * FROM users_favorite_places WHERE place_id = ?2 and user_id = (SELECT id FROM users WHERE email = ?1))", nativeQuery = true)
+    @Query(value = "SELECT EXISTS(SELECT * FROM users_favorite_places" +
+                   " WHERE place_id = ?2 and user_id = (SELECT id FROM users WHERE email = ?1))",
+            nativeQuery = true)
     boolean existsByEmailAndId(String userEmail, long placeId);
+
+    @Query(nativeQuery = true, value = "SELECT place_id FROM users_favorite_places" +
+                                       " WHERE user_id = (SELECT id FROM users WHERE email = ?1)")
+    List<Long> getAllByEmail(String email);
+
+    @Query(nativeQuery = true, value = "SELECT name FROM users_favorite_places WHERE place_id = ?1" +
+                                       " AND user_id = (SELECT id FROM users WHERE email = ?2)")
+    String getFavoritePlaceNameByIdAndEmail(long placeId, String email);
+
+    @Modifying
+    @Query(nativeQuery = true,
+            value = """
+                    UPDATE users_favorite_places SET name = ?2
+                    WHERE place_id = ?1 AND user_id = (SELECT id FROM users WHERE email = ?3)
+                   """)
+    void updateFavoritePlace(long placeId, String name, String email);
 }
