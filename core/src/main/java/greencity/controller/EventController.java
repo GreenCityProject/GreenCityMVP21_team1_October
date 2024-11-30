f10969104c24ce56d1728ef023847437a0c5f7db
Andrii-Danylenko <ineedmoreweekends@gmail.com>
2024-11-30 12:11:49 +0200

    Small code refactoring. Changed all Principal entries to @CurrentUser UserVO

--- core/src/main/java/greencity/controller/EventController.java
+++ core/src/main/java/greencity/controller/EventController.java
@@ -1,5 +1,6 @@
 package greencity.controller;
 
+import greencity.annotations.CurrentUser;
 import greencity.annotations.EventValidation;
 import greencity.constant.ErrorMessage;
 import greencity.constant.HttpStatuses;
@@ -12,7 +13,6 @@ import greencity.enums.EventStatus;
 import greencity.enums.EventType;
 import greencity.exception.exceptions.WrongIdException;
 import greencity.service.EventService;
-import greencity.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -26,14 +26,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import java.security.Principal;
-import java.util.List;
 
+import java.util.List;
 
 
 @RestController
@@ -42,7 +39,6 @@ import java.util.List;
 @Validated
 public class EventController {
     private final EventService eventService;
-    private final UserService userService;
 
     /**
      * Method for updating event
@@ -66,7 +62,7 @@ public class EventController {
     @PutMapping(value = "/{eventId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<EventResponseDto> update(
             @Parameter(required = true) @EventValidation @RequestPart EventDetailsUpdate requestDto,
-            @Parameter(hidden = true) Principal principal,
+            @Parameter(hidden = true) @CurrentUser UserVO userVO,
             @PathVariable Long eventId,
             @RequestPart(required = false) @Nullable MultipartFile[] file) {
 
@@ -74,15 +70,15 @@ public class EventController {
             throw new WrongIdException(ErrorMessage.EVENT_ID_IN_PATH_PARAM_AND_ENTITY_NOT_EQUAL);
         }
 
-        return ResponseEntity.ok().body(eventService.update(requestDto, principal.getName(), file));
+        return ResponseEntity.ok().body(eventService.update(requestDto, userVO.getName(), file));
     }
 
     /**
      * Method for deleting an event.
      * This endpoint allows an Admin or the Organizer of the event to delete it.
      *
-     * @param eventId   ID of the event to be deleted.
-     * @param principal the currently authenticated user.
+     * @param eventId ID of the event to be deleted.
+     * @param userVO  the currently authenticated user.
      * @return {@link ResponseEntity<Void>}
      * @author Belchuk Stanislav
      */
@@ -102,24 +98,15 @@ public class EventController {
     @DeleteMapping("/{eventId}")
     public ResponseEntity<Void> deleteEvent(
             @PathVariable Long eventId,
-            @Parameter(hidden = true) Principal principal) {
-        UserVO currentUser = userService.findByEmail(principal.getName());
-        Long userId = currentUser.getId();
-        eventService.deleteEvent(eventId, userId);
+            @Parameter(hidden = true) @CurrentUser UserVO userVO) {
+        eventService.deleteEvent(eventId, userVO.getId());
         return ResponseEntity.ok().build();
-    }
-
-
-    // Метод для отримання поточного користувача
-    private UserVO getCurrentUser(Principal principal) {
-        return userService.findByEmail(principal.getName());
     }
 
     /**
      * Method for getting all events related to the authenticated user.
      * This includes both events created by the user and events the user has joined.
      */
-    @GetMapping
     @Operation(summary = "Get all events created or joined by the authenticated user. Supports optional filters by event type and pagination details.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = HttpStatuses.OK,
@@ -129,19 +116,18 @@ public class EventController {
             @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST,
                     content = @Content(examples = @ExampleObject(HttpStatuses.BAD_REQUEST)))
     })
+    @GetMapping
     public ResponseEntity<PageableAdvancedDto<EventResponseDto>> getUserEvents(
-            @Parameter(hidden = true) Principal principal,
+            @Parameter(hidden = true) @CurrentUser UserVO userVO,
             @RequestParam(required = false) EventType eventType,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size,
             @RequestParam(required = false) Double userLatitude,
             @RequestParam(required = false) Double userLongitude) {
-
-        UserVO currentUser = getCurrentUser(principal);
         Pageable pageable = PageRequest.of(page, size);
 
         PageableAdvancedDto<EventResponseDto> userEvents = eventService.getUserEvents(
-                currentUser.getId(), eventType, userLatitude, userLongitude, pageable);
+                userVO.getId(), eventType, userLatitude, userLongitude, pageable);
 
         return ResponseEntity.ok(userEvents);
     }
@@ -149,7 +135,6 @@ public class EventController {
     /**
      * Method for getting all events created by the authenticated user.
      */
-    @GetMapping("/createdEvents")
     @Operation(summary = "Get all events created by the authenticated user")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = HttpStatuses.OK,
@@ -157,14 +142,12 @@ public class EventController {
             @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED,
                     content = @Content(examples = @ExampleObject(HttpStatuses.UNAUTHORIZED)))
     })
+    @GetMapping("/createdEvents")
     public ResponseEntity<MyEventsResponseDto> getCreatedEvents(
-            @Parameter(hidden = true) Principal principal,
+            @Parameter(hidden = true) @CurrentUser UserVO userVO,
             @RequestParam EventStatus status) {
-
-        UserVO currentUser = getCurrentUser(principal);
-
-        List<EventResponseDto> createdEvents = eventService.getCreatedEventsByUser(currentUser.getId(), status);
-        List<EventResponseDto> attendingEvents = eventService.getAttendingEventsByUser(currentUser.getId(), status);
+        List<EventResponseDto> createdEvents = eventService.getCreatedEventsByUser(userVO.getId(), status);
+        List<EventResponseDto> attendingEvents = eventService.getAttendingEventsByUser(userVO.getId(), status);
 
         MyEventsResponseDto response = new MyEventsResponseDto(createdEvents, attendingEvents);
 
@@ -174,7 +157,6 @@ public class EventController {
     /**
      * Method for getting all related events for the user.
      */
-    @GetMapping("/relatedEvents")
     @Operation(summary = "Get all related events for the user")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = HttpStatuses.OK,
@@ -182,12 +164,11 @@ public class EventController {
             @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED,
                     content = @Content(examples = @ExampleObject(HttpStatuses.UNAUTHORIZED)))
     })
+    @GetMapping("/relatedEvents")
     public ResponseEntity<List<EventResponseDto>> getUserRelatedEvents(
             @RequestParam("status") EventStatus status,
-            @Parameter(hidden = true) Principal principal) {
-
-        UserVO currentUser = getCurrentUser(principal);
-        List<EventResponseDto> relatedEvents = eventService.getAttendingEventsByUser(currentUser.getId(), status);
+            @Parameter(hidden = true) @CurrentUser UserVO userVO) {
+        List<EventResponseDto> relatedEvents = eventService.getAttendingEventsByUser(userVO.getId(), status);
 
         return ResponseEntity.ok(relatedEvents);
     }
